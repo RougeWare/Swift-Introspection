551e797528d256392f6d6bd4a4de058e54cd91ab
Ben Leggiero <BenLeggiero@Gmail.com>
2021-07-24 14:19:06 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introspection
+
+A description of this package.
---
+++ Tests/IntrospectionTests/IntrospectionTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Introspection
+
+final class IntrospectionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Introspection().text, "Hello, World!")
+    }
+}
