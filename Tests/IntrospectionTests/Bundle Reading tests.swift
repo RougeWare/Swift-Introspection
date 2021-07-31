7570ba6268b594233701c75c87e8a772b04e6f63
Ky Leggiero <KyLeggiero@Gmail.com>
2021-07-30 23:05:17 -0600

    Accidentally mixed up the names for two test files

--- Tests/IntrospectionTests/Bundle Reading tests.swift
+++ Tests/IntrospectionTests/Bundle Reading tests.swift
@@ -1,5 +1,5 @@
 //
-//  Device Info tests.swift
+//  Bundle Reading tests.swift
 //  Introspection
 //
 //  Created by Ky Leggiero on 2021-07-24.
@@ -11,50 +11,9 @@ import Introspection
 
 
 
-final class DeviceInfoTests: XCTestCase {
-    func testCurrentDevice() {
-        let current = Introspection.Device.current
-        XCTAssertFalse(current.modelType.withoutTypeSafety().isEmpty)
-        
-        if !current.modelType.isSimulator,
-           !current.modelType.isVirtualMachine
-        {
-            XCTAssertNotNil(current.deviceClass, "Model isn't a simulator, but device class unknown: \(current)")
-        }
-    }
-    
-    
-    func testDevicePassesThroughToModelType() {
-        let testDevice = Introspection.Device(modelType: "TestModelType", class: .desktop)
-        XCTAssertEqual(testDevice.modelType, "TestModelType")
-        XCTAssertEqual(testDevice.deviceClass, .desktop)
-        XCTAssertNil(testDevice.modelType.deviceClass)
-        XCTAssertEqual(testDevice.description, testDevice.modelType.description)
-    }
-    
-    
-    func testModelTypeInferredClassAccurate() {
-        XCTAssertEqual(Introspection.Device.macBook.deviceClass, .laptop)
-        XCTAssertEqual(Introspection.Device.macBookAir.deviceClass, .laptop)
-        XCTAssertEqual(Introspection.Device.macBookPro.deviceClass, .laptop)
-        
-        XCTAssertEqual(Introspection.Device.iMac.deviceClass, .desktop)
-        XCTAssertEqual(Introspection.Device.iMacPro.deviceClass, .desktop)
-        XCTAssertEqual(Introspection.Device.macMini.deviceClass, .desktop)
-        XCTAssertEqual(Introspection.Device.macPro.deviceClass, .desktop)
-        
-        XCTAssertEqual(Introspection.Device.iPhone.deviceClass, .phone)
-        
-        XCTAssertEqual(Introspection.Device.iPad.deviceClass, .tablet)
-        
-        XCTAssertEqual(Introspection.Device.iPod.deviceClass, .portableMusicPlayer)
-        
-        XCTAssertEqual(Introspection.Device.watch.deviceClass, .watch)
-        XCTAssertEqual(Introspection.Device.tv.deviceClass, .tvBox)
-        
-        XCTAssertNil(Introspection.Device.iPhoneSimulator_i386.deviceClass)
-        XCTAssertNil(Introspection.Device.iPhoneSimulator_x86_64.deviceClass)
-        XCTAssertNil(Introspection.Device.iPhoneSimulator_arm64.deviceClass)
+final class BundleReadingTests: XCTestCase {
+    func testAppVersion() {
+        // I'm not entirely sure how to test this, but I poked through it in the console and it seems to work well
     }
 }
 
